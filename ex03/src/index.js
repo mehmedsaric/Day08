5f29dc69de72451916b8bb1acf4f62d2ed6c063a
mehmedsaric <mehmed.saaric@gmail.com>
2021-03-15 22:18:42 +0100

    obradjen

---
+++ ex03/src/index.js
@@ -0,0 +1,18 @@
+var myGlobalVariable = 10;
+function function1() {
+    console.log("notGlobalVariable", 5);
+}
+function function2() {
+    var result = "";
+    if (typeof myGlobalVariable != "undefined"){
+result += "myGlobalVariable: " + myGlobalVariable;
+    }
+if (typeof notGlobalVariable != "undefined"){
+result += " notGlobalVariable: " + notGlobalVariable
+
+}
+console.log(result);
+}
+function1();
+function2();
+module.exports = {function1,function2};
